f340b949db160600dc5a23c8d594dd9e5b9ab9c1
Julia <julka.uujma@gmail.com>
2025-04-08 19:40:09 +0200

    add deleting and adding notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    console.log(newNote);
+    setNotes((prevValue) => [...prevValue, newNote]);
+    console.log(notes);
+  }
+  function deleteNote(id) {
+    setNotes((prevNotes) => prevNotes.filter((note, index) => index !== id));
+  }
   return (
     <>
       <Header />
-      <Note />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNote}
+        />
+      ))}
       <Footer />
     </>
   );
